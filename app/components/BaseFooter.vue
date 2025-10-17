3cb830400cb6093f303f486b91e72862a16ccd5f
pdhaneswara <pdhaneswara@gmail.com>
2025-10-17 20:45:50 +0700

    Update BaseFooter.vue

--- app/components/BaseFooter.vue
+++ app/components/BaseFooter.vue
@@ -8,17 +8,6 @@ const currentYear = ref(new Date().getFullYear());
     <footer class="py-3 bg-neutral-800 text-white">
         <nav class="flex container mx-auto items-center">
             <div><span>&#169;</span> {{ currentYear }} Orange Store. All rights reserved.</div>
-            <ul class="flex items-center gap-6 ml-auto capitalize">
-                <li>
-                <NuxtLink to="/">Privacy policy</NuxtLink>
-                </li>
-                <li>
-                <NuxtLink to="/about">Terms of Service</NuxtLink>
-                </li>
-                <li>
-                <NuxtLink to="/admin">Contact Us</NuxtLink>
-                </li>
-            </ul>
         </nav>
     </footer>
 </template>
